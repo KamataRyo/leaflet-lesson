cc2109152c88884f52ffeda3b34c2d72148a9e25
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-10-24 08:32:32 +0900

    Add scaffold script

---
+++ bin/scaffold.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo -n "project slug: (new) " && read SLUG
+echo -n "project title: (New Map) " && read TITLE
+echo -n "project description: (A new map.) " && read DESCRIPTION
+
+if [[ $SLUG == '' ]]; then
+  SLUG=new
+fi
+
+if [[ $TITLE == '' ]]; then
+  TITLE='New Map'
+fi
+
+if [[ $DESCRIPTION == '' ]]; then
+  DESCRIPTION='A new map.'
+fi
+cp -i ./render-sample.js ./src/render-$SLUG.js
+cat ./src/render-$SLUG.js | sed -e "s/{{title}}/$TITLE/g" | sed -e "s/{{description}}/$DESCRIPTION/g" > ./src/render-$SLUG.js
+
+echo "\`./src/render-$SLUG.js\` has been generated."
--- render-sample.js
+++ render-sample.js
@@ -1,5 +1,12 @@
 'use strict'
 
-export default (map, L) => {
-  // do anything with `L`(Leaflet object) and `map`!
+export default {
+
+  title: '{{title}}',
+
+  description: '{{description}}',
+
+  render: (map, L) => {
+    // do anything with `L`(Leaflet object) and `map`!
+  }
 }

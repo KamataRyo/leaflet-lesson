af05e9aaaacb1c38e7408dffa95362ec053f603c
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-10-23 17:25:09 +0900

    Some test

--- src/render-old-kyoto.js
+++ src/render-old-kyoto.js
@@ -3,8 +3,8 @@ import L from 'mapbox.js'
 
 export default (e) => {
   const map = e.target
-
-  const ENDPOINT = 'http://www.arc.ritsumei.ac.jp/archive01/theater/image/PB/geo/maps/meisai_hasegawa/{z}/{x}/{y}.png'
+  // const ENDPOINT = 'http://www.arc.ritsumei.ac.jp/archive01/theater/image/PB/geo/maps/mono_s4/{z}/{x}/{y}.png'
+  const ENDPOINT = 'http://www.arc.ritsumei.ac.jp/archive01/theater/image/PB/geo/maps/meisai_sougou/{z}/{x}/{y}.png'
   L.tileLayer(ENDPOINT).addTo(map)
 
 }

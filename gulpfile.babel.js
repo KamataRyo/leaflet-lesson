1aabf72e4b9b06393333312ce7b3be3ffa551b61
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-10-23 20:30:15 +0900

    Add map index

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,19 +7,21 @@ import source     from 'vinyl-source-stream'
 import fs         from 'fs'
 import conf from './src/pages.json'
 
+// duplicate src and generate page
 gulp.task('pagenate', () => {
   fs.readdir('./src/', (err, files) => {
     files.forEach((file) => {
       const matched = file.match(/^render\-(.*)\.js$/i)
       if (matched && matched[1] !== 'sample') {
-        gulp.src('./src/index.html.ejs')
+        // duplicate html
+        gulp.src('./src/page.html.ejs')
           .pipe(ejs(conf[matched[1]]))
           .pipe(rename((path) => {
             path.basename = 'index'
             path.extname = '.html'
           }))
           .pipe(gulp.dest(`./dest/${matched[1]}`))
-
+        // duplicate js
         gulp.src('./src/*.js')
           .pipe(rename((path) => {
             if (`${path.basename}${path.extname}` === matched[0]) {
@@ -32,9 +34,10 @@ gulp.task('pagenate', () => {
   })
 })
 
+// bundle
 gulp.task('browserify', () => {
-  fs.readdir('./dest/', (err, folders) => {
-    folders.forEach((folder) => {
+  fs.readdir('./dest/', (err, projects) => {
+    projects.forEach((folder) => {
       browserify({
         entries: [`./dest/${folder}/`]
       })
@@ -46,9 +49,19 @@ gulp.task('browserify', () => {
   })
 })
 
+// clean up and generate index
 gulp.task('afterAll', () => {
-  fs.readdir('./dest/', (err, folders) => {
-    folders.forEach((folder) => {
+  fs.readdir('./dest/', (err, projects) => {
+
+    gulp.src('src/index.html.ejs')
+      .pipe(ejs({projects, conf}))
+      .pipe(rename((path) => {
+        path.basename = 'index'
+        path.extname = '.html'
+      }))
+      .pipe(gulp.dest('./dest/'))
+
+    projects.forEach((folder) => {
       fs.readdir(`./dest/${folder}/`, (err, files) => {
         files.forEach((file) => {
           if (file !== 'app.js' && file !== 'index.html') {
--- src/render-old-kyoto.js
+++ src/render-old-kyoto.js
@@ -3,8 +3,9 @@ import L from 'mapbox.js'
 
 export default (e) => {
   const map = e.target
-  // const ENDPOINT = 'http://www.arc.ritsumei.ac.jp/archive01/theater/image/PB/geo/maps/mono_s4/{z}/{x}/{y}.png'
-  const ENDPOINT = 'http://www.arc.ritsumei.ac.jp/archive01/theater/image/PB/geo/maps/meisai_sougou/{z}/{x}/{y}.png'
-  L.tileLayer(ENDPOINT).addTo(map)
 
+  const ENDPOINT1 = 'http://www.arc.ritsumei.ac.jp/archive01/theater/image/PB/geo/maps/meisai_sougou/{z}/{x}/{y}.png'
+  const ENDPOINT2 = 'http://www.arc.ritsumei.ac.jp/archive01/theater/image/PB/geo/maps/meisai_hasegawa/{z}/{x}/{y}.png'
+  L.tileLayer(ENDPOINT1).addTo(map)
+  L.tileLayer(ENDPOINT2).addTo(map)
 }

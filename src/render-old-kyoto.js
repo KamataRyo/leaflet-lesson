c64cfd69c4df3893a4dfe88663a0a4a8e531f76f
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-10-24 15:44:54 +0900

    Fix old-kyoto map with TMS axis inversement

--- src/render-old-kyoto.js
+++ src/render-old-kyoto.js
@@ -9,7 +9,7 @@ export default {
 
     const ENDPOINT1 = 'http://www.arc.ritsumei.ac.jp/archive01/theater/image/PB/geo/maps/meisai_sougou/{z}/{x}/{y}.png'
     const ENDPOINT2 = 'http://www.arc.ritsumei.ac.jp/archive01/theater/image/PB/geo/maps/meisai_hasegawa/{z}/{x}/{y}.png'
-    L.tileLayer(ENDPOINT1).addTo(map)
-    L.tileLayer(ENDPOINT2).addTo(map)
+    L.tileLayer(ENDPOINT1, {tms: true}).addTo(map)
+    L.tileLayer(ENDPOINT2, {tms: true}).addTo(map)
   }
 }

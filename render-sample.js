b217982d783169787a5aed8b725a82bc284c24c7
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-11-23 14:27:22 +0900

    Fix render-sample

--- render-sample.js
+++ render-sample.js
@@ -6,6 +6,8 @@ export default {
 
   description: '{{description}}',
 
+  template: './src/page.html.ejs',
+
   render: (map, L) => {
     // do anything with `L`(Leaflet object) and `map`(Leaflet map object)!
   }

b4bc93da561df1c7a7da765d594c701069417e02
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-11-23 14:26:57 +0900

    Add kansai railways warped map

---
+++ src/render-kansai-railway.js
@@ -0,0 +1,17 @@
+'use strict'
+
+export default {
+
+  title: '関西路線図タイル',
+
+  description: '関西路線図にジオリファレンスを当ててタイル化したもの',
+
+  template: './src/page.html.ejs',
+
+  render: (map, L) => {
+
+    const ENDPOINT = 'http://tiles.biwako.io/kansai-railways/{z}/{x}/{y}.png'
+    L.tileLayer(ENDPOINT, {tms: true, opacity: .7}).addTo(map)
+
+  }
+}
